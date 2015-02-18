54b9dcea38a78131f92c56bfadb8145c5f723750
AdrianJakobsen <adrjak.aj@gmail.com>
2015-02-18 10:12:34 +0100

    changed mainviewController cause somebody had added pretty much the same shit in another class, and tried to fixs my tests in mainViewController, it didn't works so  well yet. Adrian fix this shit since i am the only one doing shit here

--- MyMovies/src/tests/TestMainViewController.java
+++ MyMovies/src/tests/TestMainViewController.java
@@ -1,6 +1,7 @@
 package tests;
 
 import static org.junit.Assert.*;
+import it.jtomato.gson.Movie;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -17,24 +18,22 @@ import controller.MainViewController;
 import business.MyMovie;
 //se om jeg kan forkorte testene og gjøre de mer lesbare, og sjekke om testene går rundt i ring
 public class TestMainViewController {
-
+	
+	public boolean storeCalled;
 	@Test
 	public void txtFile_testToSeIfAnTxtFileAllredyExist() throws FileNotFoundException {
 		FileReader readFavorite = new FileReader("favorite.txt");
 		assertNotNull(readFavorite);
 	}
+	
 	@Test
 	public void txtFile_AddMoiveToFavoriteTextFile() throws FileNotFoundException{
-		FileReader readFavorite = new FileReader("favorite.txt");
-		Scanner readFile = new Scanner(readFavorite);
-		MainViewController controll = new MainViewController();
-		WebMediator test = new WebMediator();
-		ArrayList<business.MyMovie> temp = test.searchMovie("kingsman");
-		MyMovie check  = temp.get(0);
-		controll.addToFavorite(check);
-		String str = readFile.nextLine();
-		assertEquals("771353298: Kingsman: The Secret Service", str);
+		MainViewController controller = new MainViewController();
+		MyMovie movie = new MyMovie(new Movie());
+		controller.addToFavorite(movie);
+		assertEquals();
 	}
+	
 	@Test
 	public void txtFile_testGetFavorites() throws FileNotFoundException{
 		FileReader readFavorite = new FileReader("favorite.txt");
@@ -48,18 +47,19 @@ public class TestMainViewController {
 		}
 		assertEquals("771353298: Kingsman: The Secret Service", controll.getFavorites());
 	}
-//	@Test
-//	public void txtFile_AddMoiveToFavoriteTextFile_addThreeMovies() throws FileNotFoundException{
-//		FileReader readFavorite = new FileReader("favorite.txt");
-//		WebRepository repo = new WebRepository();
-//		Scanner readFile = new Scanner(readFavorite);
-//		MainViewController controll = new MainViewController();
-//		WebMediator test = new WebMediator();
-//		ArrayList<MyMovie> movies = repo.getBoxOfficeMovies(3);
-//		for(MyMovie movie : movies){
-//			controll.addToFavorite(movie);
-//		}
-//		
-//		assertEquals("771353298: Kingsman: The Secret Service","");
-//	}
+	
+	@Test
+	//fiks denne testen sin assert
+	public void txtFile_AddMoiveToFavoriteTextFile_addThreeMovies() throws FileNotFoundException{
+		FileReader readFavorite = new FileReader("favorite.txt");
+		WebRepository repo = new WebRepository();
+		Scanner readFile = new Scanner(readFavorite);
+		MainViewController controll = new MainViewController();
+		WebMediator test = new WebMediator();
+		ArrayList<MyMovie> movies = repo.getBoxOfficeMovies(3);
+		for(MyMovie movie : movies){
+			controll.addToFavorite(movie);
+		}
+		assertEquals("771353298: Kingsman: The Secret Service","");
+	}
 }
